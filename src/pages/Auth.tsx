c49c6a66335f6e1b1a298675411339b358dd7020
d0kur0 <dev.dokuro@gmail.com>
2020-09-10 01:35:48 +0300

    implement this page

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,15 +1,37 @@
 import React from "react";
-import styled from "styled-components";
+import { Form, Input, Label, Button } from "../components/Forms";
+import { GiFlowerTwirl } from "react-icons/gi/index";
+import { Link } from "react-router-dom";
+import { Root, TopLogo, VKButton, Other, Title } from "../components/SignForms";
 
 function Auth() {
   return (
-    <div>
-      {"dsghsdfhsjdsfjdfjdjdsjsdjdsjdjdjdsjdfsjfgbngisdbgoag bodsa gadsoydbgodysagbdsagadsb0gdsabg0agbad0sgbadsgbdasgiuodbygpoiadsbgipobg"
-        .split("")
-        .map((_: string, key: number) => (
-          <div key={key}>test</div>
-        ))}
-    </div>
+    <Root>
+      <TopLogo>
+        <GiFlowerTwirl />
+      </TopLogo>
+
+      <Title>Авторизация</Title>
+
+      <Form>
+        <Label>
+          <Input type="email" placeholder="Email адрес" />
+        </Label>
+        <Label>
+          <Input type="password" placeholder="Ваш пароль" />
+        </Label>
+
+        <Button>Продолжить</Button>
+        <VKButton>Войти через vk</VKButton>
+
+        <Other>
+          <Link to="/registration">Регистрация аккаунта</Link>
+        </Other>
+        <Other>
+          <Link to="/passwordRecovery">Забыли пароль?</Link>
+        </Other>
+      </Form>
+    </Root>
   );
 }
 
