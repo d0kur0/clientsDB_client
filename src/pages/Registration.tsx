86f5d1f001d0651d5ffc5859314aa83925dade69
d0kur0 <dev.dokuro@gmail.com>
2020-09-10 01:36:22 +0300

    implement this page

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { Form, Input, Label, Button } from "../components/Forms";
+import { GiFlamer } from "react-icons/gi/index";
+import { Link } from "react-router-dom";
+import { Root, TopLogo, Other, Title } from "../components/SignForms";
 
-function Registration() {
-  return <div>Registration</div>;
+function Auth() {
+  return (
+    <Root>
+      <TopLogo>
+        <GiFlamer />
+      </TopLogo>
+
+      <Title>Регистрация</Title>
+
+      <Form autoComplete="off">
+        <Label>
+          <Input type="email" placeholder="Email адрес" />
+        </Label>
+        <Label>
+          <Input type="password" placeholder="Ваш пароль" />
+        </Label>
+        <Label>
+          <Input type="password" placeholder="Повторите пароль" />
+        </Label>
+
+        <Button>Продолжить</Button>
+
+        <Other>
+          <Link to="/auth">Авторизация</Link>
+        </Other>
+      </Form>
+    </Root>
+  );
 }
 
-export default Registration;
+export default Auth;
